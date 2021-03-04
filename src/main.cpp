a7577c79ccc1f22ede2aeb363dd962430b68cb67
eraulhe <raul.hernandez.sucuesta@ericsson.com>
2021-03-04 17:10:36 +0100

    CODE: first template version added on top of previous cmake project

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,141 @@
 #include <division.h>
+
+#include <optional>
+#include <string>
 #include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include <type_traits>
 
-using namespace std;
+// Check value is of a type
+//
+template <typename T>
+struct AlwaysFalse : std::false_type
+{
+};
 
-static const char *const HEADER = "\nDivider © 2018 Monkey Claps Inc.\n\n";
-static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\n\nDescription:\n\tComputes the result of a fractional division,\n\tand reports both the result and the remainder.\n";
+template<typename T> bool IsJson(const rapidjson::Value& v) {
+  static_assert(AlwaysFalse<T>::value, "Type not supported"); }
 
-int main(int argc, const char *argv[]) {
-  Fraction f;
+template<> bool IsJson<bool>(const rapidjson::Value& v ) {
+  return v.IsBool(); }
+template<> bool IsJson<int32_t>(const rapidjson::Value& v ) {
+  return v.IsInt(); }
+template<> bool IsJson<int64_t>(const rapidjson::Value& v ) {
+  return v.IsInt64(); }
+template<> bool IsJson<uint32_t>(const rapidjson::Value& v) {
+  return v.IsUint(); }
+template<> bool IsJson<uint64_t>(const rapidjson::Value& v) {
+  return v.IsUint64(); }
+template<> bool IsJson<float>(const rapidjson::Value& v) {
+  return v.IsDouble(); }
+template<> bool IsJson<double>(const rapidjson::Value& v) {
+  return v.IsDouble(); }
+template<> bool IsJson<std::string>(const rapidjson::Value& v) {
+  return v.IsString(); }
 
-  cout << HEADER;
+// Get the value as a type
+//
+template<typename T> T GetJson(const rapidjson::Value& v) {
+  static_assert(AlwaysFalse<T>::value, "Type not supported"); }
+template<> bool GetJson<bool>(const rapidjson::Value& v) {
+  return v.GetBool(); }
+template<> int32_t GetJson<int32_t>(const rapidjson::Value& v) {
+  return v.GetInt(); }
+template<> uint32_t GetJson<uint32_t>(const rapidjson::Value& v) {
+  return v.GetUint(); }
+template<> int64_t GetJson<int64_t>(const rapidjson::Value& v) {
+  return v.GetInt64(); }
+template<> uint64_t GetJson<uint64_t>(const rapidjson::Value& v) {
+  return v.GetUint64(); }
+template<> float GetJson<float>(const rapidjson::Value& v) {
+  return v.GetDouble(); }
+template<> double GetJson<double>(const rapidjson::Value& v) {
+  return v.GetDouble(); }
+template<> std::string GetJson<std::string>(const rapidjson::Value& v) {
+  return v.GetString(); }
 
-  // ensure the correct number of parameters are used.
-  if (argc < 3) {
-    cout << USAGE;
-    return 1;
-  }
+// Check optional
+//
+// template< typename T >
+// struct IsOptional : public std::false_type {
+// };
 
-  f.numerator = atoll(argv[1]);
-  f.denominator = atoll(argv[2]);
+// template< typename T >
+// struct IsOptional< std::optional< T > > : public std::true_type {
+// };
 
-  Division d = Division(f);
-  try {
-    DivisionResult r = d.divide();
+// template< typename T >
+// constexpr bool IsOptionalValue = IsOptional< T >::value;
 
-    cout << "Division : " << f.numerator << " / " << f.denominator << " = " << r.division << "\n";
-    cout << "Remainder: " << f.numerator << " % " << f.denominator << " = " << r.remainder << "\n";
-  } catch (DivisionByZero) {
-    cout << "Can not divide by zero, Homer. Sober up!\n";
-  }
-  return 0;
+template<template<class...> class tmpl, typename T>
+struct _is_template_for : public std::false_type {};
+
+template<template<class...> class tmpl, class... Args>
+struct _is_template_for<tmpl, tmpl<Args...>> : public std::true_type {};
+
+template<template<class...> class tmpl, typename... Ts>
+using is_template_for = std::conjunction<_is_template_for<tmpl, std::decay_t<Ts>>...>;
+
+template<template<class...> class tmpl, typename... Ts>
+constexpr bool is_template_for_v = is_template_for<tmpl, Ts...>::value;
+
+// function that tries to obtain a value with the given name
+//
+// template <typename Element, typename Target, typename TargetOptionalOf = std::result_of_t<Target(void)>::value_type > >
+// template <typename Element, typename Target, typename TargetOptionalOf = 
+// template <typename Element, typename Target, typename TargetOptionalOf = Target >
+
+//You can change the `std::result_of_t<...>` for `typename std::result_of<...>::type` to make it work in C++11 :)
+
+template <typename Element, typename Target >
+bool GetJsonValue(const Element &element, const char* name, Target& target) {
+
+  //   typename std::conditional<std::is_void<T>::value, _X<> : _X<T>>::type
+  if constexpr(is_template_for_v<std::optional, Target>)
+  {
+        typedef typename Target::value_type TargetOfOptional;
+        auto it = element.FindMember(name);
+        if (it != element.MemberEnd() && IsJson<TargetOfOptional>(it->value))
+          {
+            target = std::move(GetJson<TargetOfOptional>(it->value));
+            return true;
+          }
+        return false;
+   }
+   else
+     {
+       // static_assert(!is_template_for_v<std::optional, Target>, "Optional type not supported" );
+       auto it = element.FindMember(name);
+       if (it != element.MemberEnd() && IsJson<Target>(it->value))
+         {
+           target = std::move(GetJson<Target>(it->value));
+           return true;
+         }
+       return false;
+     }
+}
+
+
+
+
+int main() {
+
+  std::string jsonString("{\"id\":\"abc\",\"number\":123}");
+  rapidjson::Document doc;
+  doc.Parse(jsonString.c_str());
+ uint32_t number;
+ GetJsonValue(doc,"number",number);
+ std::cout << number << std::endl;
+
+ std::string id;
+ GetJsonValue(doc,"id",id);
+ std::cout << id << std::endl;
+ 
+ std::optional<std::string> idOpt;
+ GetJsonValue(doc,"id",idOpt);
+ std::cout << idOpt.value() << std::endl;
+
+ return 0;
 }
